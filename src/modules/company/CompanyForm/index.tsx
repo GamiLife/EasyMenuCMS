460e518006baa960226986e77f7aff303102b6fe
Cesar Jesus Alberto Alvan Huarcaya <cesar.alvan@globant.com>
2023-03-04 20:15:50 -0500

    feat: Initial Part related company social networks :sparkles:

--- src/modules/company/CompanyForm/index.tsx
+++ src/modules/company/CompanyForm/index.tsx
@@ -1,7 +1,6 @@
 import { Container, Form, Input, TextArea } from '@gamiui/standard';
 import * as S from '../../../../styles/common/resource-form';
 import { ICreateRenderForm } from '../../../common/resources';
-import { SiteEditor } from '../../site-editor';
 import * as OwnS from './styles';
 
 export type ICompanyForm = {} & Omit<ICreateRenderForm, 'handleValidate'>;
@@ -32,6 +31,23 @@ export const CompanyForm = ({ form, handleSubmit }: ICompanyForm) => {
       </Form.Item>
       <Container width="full">
         <OwnS.BrandTitle level="h4">MARCA:</OwnS.BrandTitle>
+        <Form.Item
+          rules={[{ type: 'required', message: 'Campo requerido' }]}
+          label="Meta Title"
+          name="metaTitle"
+        >
+          <Input placeholder="Meta Title" width="full" />
+        </Form.Item>
+        <Form.Item
+          rules={[{ type: 'required', message: 'Campo requerido' }]}
+          label="Meta Description"
+          name="metaDescription"
+        >
+          <Input placeholder="Meta Description" width="full" />
+        </Form.Item>
+      </Container>
+      <Container width="full">
+        <OwnS.BrandTitle level="h4">REDES SOCIALES:</OwnS.BrandTitle>
         <Form.Item
           rules={[{ type: 'required', message: 'Campo requerido' }]}
           label="Meta Title"
